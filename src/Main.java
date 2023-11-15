b7462c465f2c0832ffd422c7d9cb69c4df32ffb1
Sergey Ponomarev <itsergeyp@gmail.com>
2023-11-15 21:08:43 +1000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Netology");
+        System.out.println("2+3=");
+        System.out.println(2+3);
+
+        int speed = 18;
+        int x = speed + 3;
+
+        double d = 6.5;
+
+        String name = "Vasia";
+
+        System.out.println(name + " ехал со скоростью " + speed);
+    }
+}
